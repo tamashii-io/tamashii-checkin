3b6346576bb8e44e94e4cc7adaf77ed12cefec7d
蒼時弦也 <elct9620@frost.tw>
2017-03-17 14:43:51 +0800

    Add .gitlab-ci.yml

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 Rails.application.configure do
   # Make javascript_pack_tag load assets from webpack-dev-server.
   # config.x.webpacker[:dev_server_host] = "http://localhost:8080"
@@ -16,7 +17,7 @@ Rails.application.configure do
   config.consider_all_requests_local = true
 
   # Enable/disable caching. By default caching is disabled.
-  if Rails.root.join('tmp/caching-dev.txt').exist?
+  if Rails.root.join('tmp', 'caching-dev.txt').exist?
     config.action_controller.perform_caching = true
 
     config.cache_store = :memory_store
