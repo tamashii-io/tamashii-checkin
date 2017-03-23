d7a52696e296be8f305f3cb80e0e073f49a2b0bf
Chelong <chelong821204@gmail.com>
2017-03-23 18:42:05 +0800

    修正rspec錯誤

--- db/schema.rb
+++ db/schema.rb
@@ -15,28 +15,6 @@ ActiveRecord::Schema.define(version: 20170323030841) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "attendees", force: :cascade do |t|
-    t.integer "serial"
-    t.string "code"
-    t.string "name"
-    t.string "email"
-    t.string "phone"
-    t.string "card_serial"
-    t.text "meta"
-    t.integer "event_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "events", force: :cascade do |t|
-    t.string "name"
-    t.datetime "start_at"
-    t.datetime "end_at"
-    t.text "options"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "machines", force: :cascade do |t|
     t.string "serial"
     t.string "name"
--- spec/models/machine_spec.rb
+++ spec/models/machine_spec.rb
@@ -2,5 +2,4 @@
 require 'rails_helper'
 
 RSpec.describe Machine, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
 end
