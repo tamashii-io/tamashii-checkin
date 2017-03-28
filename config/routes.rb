08d6ac6792c102f3a36ff51882a75c8b35584212
Chelong <chelong821204@gmail.com>
2017-03-28 20:17:05 +0800

    fix rubocop error

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 # frozen_string_literal: true
 Rails.application.routes.draw do
   root 'home#index'
-<<<<<<< HEAD
   resources :check_points do
-=======
-
+  end
   resources :machines do
->>>>>>> develop
   end
 end
