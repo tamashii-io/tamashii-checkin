9726e77b0894a2ab82f5c825f94887b8135e1d82
Chelong <chelong821204@gmail.com>
2017-04-12 18:02:58 +0800

    add describe '#checkin'

--- spec/models/check_point_spec.rb
+++ spec/models/check_point_spec.rb
@@ -2,21 +2,23 @@
 require 'rails_helper'
 
 RSpec.describe CheckPoint, type: :model do
-  it '#checkin no check_record' do
-    checkpoint = create(:check_point)
-    attendee = create(:attendee)
-    expect { checkpoint.checkin(attendee.id) }.to change { CheckRecord.count }.by(1)
-  end
+  describe '#checkin' do
+    it '#checkin no check_record' do
+      checkpoint = create(:check_point)
+      attendee = create(:attendee)
+      expect { checkpoint.checkin(attendee.id) }.to change { CheckRecord.count }.by(1)
+    end
 
-  it '#checkin has check_record exclude -INFINITY..specific.ago' do
-    checkpoint = create(:check_point)
-    checkrecord = create(:check_record, check_point_id: checkpoint.id)
-    expect { checkpoint.checkin(checkrecord.attendee_id) }.to change { CheckRecord.first.times }.by(1)
-  end
+    it '#checkin has check_record exclude -INFINITY..specific.ago' do
+      checkpoint = create(:check_point)
+      checkrecord = create(:check_record, check_point_id: checkpoint.id)
+      expect { checkpoint.checkin(checkrecord.attendee_id) }.to change { CheckRecord.first.times }.by(1)
+    end
 
-  it '#checkin has check_record include -INFINITY..specific.ago' do
-    checkpoint = create(:check_point)
-    checkrecord = create(:check_record, check_point_id: checkpoint.id, updated_at: 10.days.ago)
-    expect { checkpoint.checkin(checkrecord.attendee_id) }.to change { CheckRecord.count }.by(1)
+    it '#checkin has check_record include -INFINITY..specific.ago' do
+      checkpoint = create(:check_point)
+      checkrecord = create(:check_record, check_point_id: checkpoint.id, updated_at: 10.days.ago)
+      expect { checkpoint.checkin(checkrecord.attendee_id) }.to change { CheckRecord.count }.by(1)
+    end
   end
 end
