19b7c0bbaca58689ff9f6ceceb7dbb527b8d732a
Chelong <chelong821204@gmail.com>
2017-04-12 17:59:06 +0800

    add checkpoint_model check-in method and instance method rspec

--- app/models/check_point.rb
+++ app/models/check_point.rb
@@ -5,4 +5,25 @@ class CheckPoint < ApplicationRecord
   has_many :check_record
   belongs_to :event
   belongs_to :machine
+
+  def checkin(attendee_id)
+    @record = CheckRecord.where(attendee_id: attendee_id, check_point_id: id)
+    if @record.blank?
+      CheckRecord.create(attendee_id: attendee_id, check_point_id: id)
+    else
+      in_time_range?(attendee_id)
+    end
+  end
+
+  private
+
+  def in_time_range?(attendee_id)
+    time_range = (-DateTime::Infinity.new.to_f..5.days.ago.to_f)
+
+    if time_range.include?(@record.pluck(:updated_at).last.to_f)
+      CheckRecord.create(attendee_id: attendee_id, check_point_id: id)
+    else
+      @record[0].increment
+    end
+  end
 end
