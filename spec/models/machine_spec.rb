6735b1348032de5ca4c8fa14b8a5a09b82eb584a
Chelong <chelong821204@gmail.com>
2017-04-12 12:22:39 +0800

    1. create machine model rspec

--- spec/models/machine_spec.rb
+++ spec/models/machine_spec.rb
@@ -2,4 +2,9 @@
 require 'rails_helper'
 
 RSpec.describe Machine, type: :model do
+  it 'has many check_point' do
+    machine = create(:machine)
+    create(:check_point, machine_id: machine.id)
+    expect(Machine.first.check_point).to include(CheckPoint.first)
+  end
 end
