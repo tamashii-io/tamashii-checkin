cda51daf43728cb1d28c19112a82a2b9e837b576
Chelong <chelong821204@gmail.com>
2017-04-12 12:06:30 +0800

    1. create event model rspec

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -2,4 +2,15 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
+  it 'has many attendee' do
+    event = create(:event)
+    create(:attendee, event_id: event.id)
+    expect(Event.first.attendee).to include(Attendee.first)
+  end
+
+  it 'has many check_point' do
+    event = create(:event)
+    create(:check_point, event_id: event.id)
+    expect(Event.first.check_point).to include(CheckPoint.first)
+  end
 end
