ac5eed46a0d173783f50b499f7d21cc0c19105e2
Chelong <chelong821204@gmail.com>
2017-04-14 16:29:59 +0800

    1. use shoulda to rewrite attendee rspec association(has many, belongs to) 2. delete not relate attendee rspec

--- spec/models/attendee_spec.rb
+++ spec/models/attendee_spec.rb
@@ -2,14 +2,7 @@
 require 'rails_helper'
 
 RSpec.describe Attendee, type: :model do
-  it 'belongs to event' do
-    create(:attendee)
-    expect(Attendee.first.event).to eq(Event.first)
-  end
+  it { should have_many(:check_record) }
 
-  it 'has many check_record' do
-    attendee = create(:attendee)
-    create(:check_record, attendee_id: attendee.id)
-    expect(Attendee.first.check_record).to include(CheckRecord.first)
-  end
+  it { should belong_to(:event) }
 end
