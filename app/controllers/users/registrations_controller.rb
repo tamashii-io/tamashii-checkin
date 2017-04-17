6911edb43a65f42fdb5e37385605f22ea5435b89
蒼時弦也 <elct9620@frost.tw>
2017-04-17 11:53:57 +0800

    Apply theme for edit registeration

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 class Users::RegistrationsController < Devise::RegistrationsController
-  layout 'session'
+  layout :pick_layout
+
+  private
+
+  def pick_layout
+    return 'session' if %w(new).include?(params[:action])
+  end
 end
