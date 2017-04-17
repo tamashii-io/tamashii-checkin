347d444d8f517a759b40e69b955a9280971d7daf
Chelong <chelong821204@gmail.com>
2017-04-17 12:20:15 +0800

    update routes check_records

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
   devise_for :users
 
   root 'home#index'
-  resources :check_records
   resources :check_points
   resources :machines
   resources :events do
     resources :attendees
+    resources :check_records
   end
 end
