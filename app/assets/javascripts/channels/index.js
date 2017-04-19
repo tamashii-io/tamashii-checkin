d37180c32be5b9599c6363ee21d7bcd7943c9fb0
Chelong <chelong821204@gmail.com>
2017-04-19 11:32:10 +0800

    add index channel

---
+++ app/assets/javascripts/channels/index.js
@@ -0,0 +1,13 @@
+App.index = App.cable.subscriptions.create("IndexChannel", {
+  connected: function() {
+    // Called when the subscription is ready for use on the server
+  },
+
+  disconnected: function() {
+    // Called when the subscription has been terminated by the server
+  },
+
+  received: function(data) {
+    // Called when there's incoming data on the websocket for this channel
+  }
+});
