39fa645187dd4517a9aee1dca7c4fd34b9fe26bf
shes50103 <zhansheng@zhanshengdeMacBook-Air.local>
2017-04-19 14:33:12 +0800

    def machine_available & rspec

--- app/models/machine.rb
+++ app/models/machine.rb
@@ -2,4 +2,5 @@
 # missing top-level class documentation comment
 class Machine < ApplicationRecord
   has_many :check_points
+  has_many :events, through: :check_points
 end
--- spec/models/check_point_spec.rb
+++ spec/models/check_point_spec.rb
@@ -15,4 +15,55 @@ RSpec.describe CheckPoint, type: :model do
       expect { subject.checkin(checkrecord.attendee_id) }.to change { CheckRecord.first.times }.by(1)
     end
   end
+
+  let(:machine) { create(:machine) }
+  let(:event) { create(:event) }
+  let(:check_point) { create(:check_point, name: 'name', machine_id: machine.id, event_id: event.id) }
+
+  it 'validates title' do
+    expect(build(:check_point, name: nil, machine_id: machine.id, event_id: event.id)).not_to be_valid
+    expect(build(:check_point, name: 'name', machine_id: machine.id, event_id: event.id)).to be_valid
+  end
+
+  it 'belongs to machines' do
+    expect(machine.check_points).to include(check_point)
+  end
+
+  it 'belongs to events' do
+    expect(event.check_points).to include(check_point)
+  end
+
+  describe 'machine_available_test' do
+    let(:machines) { create_list(:machine, 2) }
+    let(:event_a) { create(:event, name: 'name', start_at: '2015-04-11 09:27:00', end_at: '2015-04-13 09:27:00') }
+    let(:event_b) { create(:event, name: 'name', start_at: '2015-04-14 09:27:00', end_at: '2015-04-16 09:27:00') }
+    let(:event_c) { create(:event, name: 'name', start_at: '2015-04-15 09:27:00', end_at: '2015-04-20 09:27:00') }
+
+    it 'available machine' do
+      expect(build(:check_point, name: 'name', machine_id: machines[0].id, event_id: event_a.id)).to be_valid
+    end
+
+    it 'reuse machine' do
+      create(:check_point, name: 'name', machine_id: machines[0].id, event_id: event_a.id)
+      expect(build(:check_point, name: 'name', machine_id: machines[0].id, event_id: event_a.id)).not_to be_valid
+    end
+
+    it 'two event separate' do
+      create(:check_point, name: 'name', machine_id: machines[0].id, event_id: event_a.id)
+      expect(build(:check_point, name: 'name', machine_id: machines[0].id, event_id: event_b.id)).to be_valid
+    end
+
+    it 'two event overlap' do
+      create(:check_point, name: 'name', machine_id: machines[0].id, event_id: event_b.id)
+      expect(build(:check_point, name: 'name', machine_id: machines[0].id, event_id: event_c.id)).not_to be_valid
+      expect(build(:check_point, name: 'name', machine_id: machines[1].id, event_id: event_c.id)).to be_valid
+    end
+
+    it 'three event overlap' do
+      create(:check_point, name: 'name', machine_id: machines[0].id, event_id: event_b.id)
+      expect(build(:check_point, name: 'name', machine_id: machines[0].id, event_id: event_a.id)).to be_valid
+      expect(build(:check_point, name: 'name', machine_id: machines[0].id, event_id: event_b.id)).not_to be_valid
+      expect(build(:check_point, name: 'name', machine_id: machines[0].id, event_id: event_c.id)).not_to be_valid
+    end
+  end
 end
