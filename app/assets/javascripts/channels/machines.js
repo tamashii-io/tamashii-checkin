28f4444ce81322895e50e76136a8b591ceefb5fe
蒼時弦也 <elct9620@frost.tw>
2017-04-19 14:14:54 +0800

    Fix javascript errors

--- app/assets/javascripts/channels/machines.js
+++ app/assets/javascripts/channels/machines.js
@@ -1,23 +1,26 @@
-App.machines = App.cable.subscriptions.create("MachinesChannel", {
-  connected: function() {
+App.machines = App.cable.subscriptions.create('MachinesChannel', {
+  connected: () => {
     // Called when the subscription is ready for use on the server
   },
 
-  disconnected: function() {
+  disconnected: () => {
     // Called when the subscription has been terminated by the server
   },
 
-  received: function(data) {
-      // TODO: Refactor to React Component
-      switch(data.event) {
-          case 'LAST_ACTIVE_UPDATED': {
-              $(`#${data.serial} .time`).text((new Date(data.last_active)).toLocaleString());
-              break;
-          }
-          case 'SHUTDOWN': {
-              $(`#${data.serial} .time`).text('');
-              break;
-          }
+  received: (data) => {
+    // TODO: Refactor to React Component
+    switch (data.event) {
+      case 'LAST_ACTIVE_UPDATED': {
+        $(`#${data.serial} .time`).text((new Date(data.last_active)).toLocaleString());
+        break;
       }
-  }
+      case 'SHUTDOWN': {
+        $(`#${data.serial} .time`).text('');
+        break;
+      }
+      default: {
+        break;
+      }
+    }
+  },
 });
