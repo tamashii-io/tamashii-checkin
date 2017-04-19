afb184c7828df7e84b3fccc2b02023dc9c249bae
shes50103 <zhansheng@zhanshengdeMacBook-Air.local>
2017-04-19 12:25:03 +0800

    machine find check_point

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,4 +4,6 @@ class Event < ApplicationRecord
   validates :name, presence: true
   has_many :attendees
   has_many :check_points
+  has_many :machines, through: :check_points
+  scope :now, -> { where('? BETWEEN start_at AND end_at', Time.zone.now) }
 end
