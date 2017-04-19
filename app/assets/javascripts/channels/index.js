60e878d9b1cc2ed75a64d359da76164822447e74
Chelong <chelong821204@gmail.com>
2017-04-19 16:30:02 +0800

    fix eslint

--- app/assets/javascripts/channels/index.js
+++ app/assets/javascripts/channels/index.js
@@ -1,14 +1,14 @@
-App.index = App.cable.subscriptions.create("IndexChannel", {
-  connected: function() {
+App.index = App.cable.subscriptions.create('IndexChannel', {
+  connected: () => {
     // Called when the subscription is ready for use on the server
   },
 
-  disconnected: function() {
+  disconnected: () => {
     // Called when the subscription has been terminated by the server
   },
 
-  received: function(data) {
+  received: (data) => {
     // Called when there's incoming data on the websocket for this channel
-    $('#machine_num').text(data.machine_nums)
-  }
+    $('#machine_num').text(data.machine_nums);
+  },
 });
