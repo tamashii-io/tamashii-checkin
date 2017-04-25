81d3c63e649bfb691b51c4bfdccb12389a18ce76
蒼時弦也 <elct9620@frost.tw>
2017-04-25 12:04:37 +0800

    Add secrets for staging config

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,8 @@ ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 # Default value for :linked_files is []
 append :linked_files,
        'config/database.yml',
-       'config/settings.yml'
+       'config/settings.yml',
+       'config/secrets.yml'
 
 # Default value for linked_dirs is []
 # append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+set :rails_env, 'production'
+
 set :deploy_to, '/home/deploy/tamashii-staging.5xruby.tw'
 role :app, %w(deploy@10.128.128.153)
 role :web, %w(deploy@10.128.128.153)
