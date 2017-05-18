1a95cef91ff9607bd256d55ab70f708156969528
Chelong <chelong821204@gmail.com>
2017-05-18 17:14:39 +0800

    add event model rspec

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -4,4 +4,38 @@ require 'rails_helper'
 RSpec.describe Event, type: :model do
   it { should have_many(:attendees) }
   it { should have_many(:check_points) }
+  it { should have_many(:check_records) }
+  it { should have_many(:machines) }
+  it { should have_many(:user_event_relationships) }
+  it { should have_many(:staffs) }
+
+  it 'is not valid without name' do
+    subject { create(:event, name: nil) }
+    expect(subject).to_not be_valid
+  end
+
+  it '#peroid' do
+    subject { create(:event) }
+    expect(subject.peroid).to eq(Range.new(subject.start_at, subject.end_at))
+  end
+
+  it '#to_s' do
+    subject { create(:event) }
+    expect(subject.to_s).to eq(subject.name)
+  end
+
+  it '.now' do
+    expired_event = create(:event, start_at: 4.days.ago, end_at: 1.day.ago)
+    event = create(:event, start_at: 4.days.ago, end_at: Time.zone.now + 1.day)
+    expect(Event.now).to include(event)
+    expect(Event.now).not_to include(expired_event)
+  end
+
+  it '.overlap' do
+    event1 = create(:event, start_at: 10.days.ago, end_at: 7.days.ago)
+    event2 = create(:event, start_at: 2.days.ago, end_at: Time.zone.now + 1.day)
+    range = Range.new(Time.zone.now, Time.zone.now + 5.days)
+    expect(Event.overlap(range)).to include(event2)
+    expect(Event.overlap(range)).not_to include(event1)
+  end
 end
