085967307563c38edc71da99dd57011ddff2164f
Chelong <chelong821204@gmail.com>
2017-05-19 11:57:52 +0800

    fix rubocop

--- spec/models/attendee_spec.rb
+++ spec/models/attendee_spec.rb
@@ -6,12 +6,11 @@ RSpec.describe Attendee, type: :model do
   it { should belong_to(:event) }
 
   let!(:attendee) { create(:attendee) }
-  let!(:attendee_card_serial) { create(:attendee, card_serial: "1234") }
+  let!(:attendee_card_serial) { create(:attendee, card_serial: '1234') }
 
   it '.not_checked_in' do
     expect(Attendee.not_checked_in).to eq(1)
   end
-
 
   it '#to_s' do
     expect(attendee.to_s).to eq(attendee.name)
