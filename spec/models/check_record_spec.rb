ed69f174ce93d28ef08028aec47bb4158ed29564
Chelong <chelong821204@gmail.com>
2017-05-19 15:56:24 +0800

    add check record model rspec

--- spec/models/check_record_spec.rb
+++ spec/models/check_record_spec.rb
@@ -7,7 +7,19 @@ RSpec.describe CheckRecord, type: :model do
   it { should belong_to(:check_point) }
 
   subject { create(:check_record) }
+  it '.active' do
+    expect(CheckRecord.active).to include(subject)
+    subject.update_attributes(updated_at: 5.days.ago)
+    expect(CheckRecord.active).not_to include(subject)
+  end
+
   it '#increment' do
     expect { subject.increment }.to change { subject.times }.by(1)
+  end
+
+  it '#to_json' do
+    json = subject.to_json
+    expect(json["check_point"]).to eq(subject.check_point)
+    expect(json["attendee"]).to eq(subject.attendee)
   end
 end
