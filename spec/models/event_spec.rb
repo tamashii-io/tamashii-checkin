d0f932efd11a7edcb9840b97b04ba28d3c464bdd
Chelong <chelong821204@gmail.com>
2017-05-23 10:49:21 +0800

    use shoulda to test validation, divide test tasks to test

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -8,13 +8,10 @@ RSpec.describe Event, type: :model do
   it { should have_many(:machines) }
   it { should have_many(:user_event_relationships) }
   it { should have_many(:staffs) }
+  it { should validate_presence_of(:name) }
 
   subject { create(:event) }
   let(:event_without_name) { Event.new(name: nil) }
-
-  it 'is not valid without name' do
-    expect(event_without_name).to_not be_valid
-  end
 
   it '#peroid' do
     expect(subject.peroid).to eq(Range.new(subject.start_at, subject.end_at))
@@ -24,18 +21,29 @@ RSpec.describe Event, type: :model do
     expect(subject.to_s).to eq(subject.name)
   end
 
-  it '.now' do
-    expired_event = create(:event, start_at: 4.days.ago, end_at: 1.day.ago)
-    event = create(:event, start_at: 4.days.ago, end_at: Time.zone.now + 1.day)
-    expect(Event.now).to include(event)
-    expect(Event.now).not_to include(expired_event)
+  describe '.now' do
+    it 'not include expired event' do
+      expired_event = create(:event, start_at: 4.days.ago, end_at: 1.day.ago)
+      expect(Event.now).not_to include(expired_event)
+    end
+
+    it 'include activity event' do
+      event = create(:event, start_at: 4.days.ago, end_at: Time.zone.now + 1.day)
+      expect(Event.now).to include(event)
+    end
   end
 
-  it '.overlap' do
-    event1 = create(:event, start_at: 10.days.ago, end_at: 7.days.ago)
-    event2 = create(:event, start_at: 2.days.ago, end_at: Time.zone.now + 1.day)
-    range = Range.new(Time.zone.now, Time.zone.now + 5.days)
-    expect(Event.overlap(range)).to include(event2)
-    expect(Event.overlap(range)).not_to include(event1)
+  describe '.overlap' do
+    it 'not in the range' do
+      event = create(:event, start_at: 10.days.ago, end_at: 7.days.ago)
+      range = Range.new(Time.zone.now, Time.zone.now + 5.days)
+      expect(Event.overlap(range)).not_to include(event)
+    end
+
+    it 'in the range' do
+      event = create(:event, start_at: 2.days.ago, end_at: Time.zone.now + 1.day)
+      range = Range.new(Time.zone.now, Time.zone.now + 5.days)
+      expect(Event.overlap(range)).to include(event)
+    end
   end
 end
