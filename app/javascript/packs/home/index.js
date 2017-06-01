9bdca2d6e8f0a359955c43b9d4757e5f170e7a2e
蒼時弦也 <elct9620@frost.tw>
2017-06-01 14:47:24 +0800

    Fix javascript syntax problem

--- app/javascript/packs/home/index.js
+++ app/javascript/packs/home/index.js
@@ -2,7 +2,7 @@ import { MachineChannel } from '../channels';
 
 class Home {
   constructor() {
-    MachineChannel.onReceived(action => this.update(action))
+    MachineChannel.onReceived(action => this.update(action));
 
     this.activated = false;
     this.total_machines = 0;
@@ -11,7 +11,7 @@ class Home {
 
   on() {
     this.activated = true;
-    this.total_machines = parseInt(document.querySelector('#machine_num').innerText);
+    this.total_machines = parseInt(document.querySelector('#machine_num').innerText, 10);
     this.$machine_status_bar = document.querySelector('#machine_num_bar .progress .progress-bar');
   }
 
@@ -20,15 +20,15 @@ class Home {
   }
 
   update(action) {
-    if(!this.activated) {
+    if (!this.activated) {
       return;
     }
 
-    if(action.total != undefined) {
-      const percent = action.total / this.total_machines * 100;
-      this.$machine_status_bar.style = 'width: ' + percent + '%';
+    if (action.total !== undefined) {
+      const percent = (action.total / this.total_machines) * 100;
+      this.$machine_status_bar.style = `width: ${percent}%`;
     }
   }
 }
 
-export default new Home;
+export default new Home();
