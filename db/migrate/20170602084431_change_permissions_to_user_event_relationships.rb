4fc092d5defb88eb64b373db39455662fec859cf
詹昇 <zhansheng@zhanshengdeMacBook-Air.local>
2017-06-02 16:50:27 +0800

    rename_column permissions

---
+++ db/migrate/20170602084431_change_permissions_to_user_event_relationships.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+# Create User Event Relationships
+class ChangePermissionsToUserEventRelationships < ActiveRecord::Migration[5.1]
+  def change
+    rename_column :user_event_relationships, :authority, :permissions
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170602071234) do
+ActiveRecord::Schema.define(version: 20170602084431) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,7 +67,7 @@ ActiveRecord::Schema.define(version: 20170602071234) do
     t.integer "role"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.json "authority"
+    t.json "permissions"
     t.index ["event_id"], name: "index_user_event_relationships_on_event_id"
     t.index ["user_id"], name: "index_user_event_relationships_on_user_id"
   end
