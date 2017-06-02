244f5fd9bc68a31d7374ceefc8a7c36048d8b0b2
詹昇 <zhansheng@zhanshengdeMacBook-Air.local>
2017-06-02 15:32:18 +0800

    rubocop

--- db/migrate/20170602071202_add_admin_to_users.rb
+++ db/migrate/20170602071202_add_admin_to_users.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+# Create User Event Relationships
 class AddAdminToUsers < ActiveRecord::Migration[5.1]
   def change
-  	add_column :users, :admin, :boolean
+    add_column :users, :admin, :boolean
   end
 end
