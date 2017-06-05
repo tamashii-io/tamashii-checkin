9e9c6ecfd32df69493b4bc793fe29c8b3f34219d
蒼時弦也 <elct9620@frost.tw>
2017-06-05 15:28:52 +0800

    Fix CheckRecord#to_json has been override

--- app/channels/accesses_channel.rb
+++ app/channels/accesses_channel.rb
@@ -12,12 +12,12 @@ class AccessesChannel < ApplicationCable::Channel
   class << self
     def update(check_record)
       user = check_record.check_point.registrar
-      broadcast_to([user, check_record.check_point], type: EVENTS[:update], record: check_record.to_json)
+      broadcast_to([user, check_record.check_point], type: EVENTS[:update], record: check_record.as_json(include: :attendee))
     end
 
     def set(check_record)
       user = check_record.check_point.registrar
-      broadcast_to([user, check_record.check_point], type: EVENTS[:set], record: check_record.to_json)
+      broadcast_to([user, check_record.check_point], type: EVENTS[:set], record: check_record.as_json(include: :attendee))
     end
 
     def request(check_point, attendee)
