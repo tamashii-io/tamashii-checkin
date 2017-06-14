828962647c716cdb22cbb7f7796ff7dd9fd903ab
Chelong <chelong821204@gmail.com>
2017-06-14 11:29:13 +0800

    1. use rename authorize name 2. modified default permissions 3. create check_point only can assign have check_point_staff permission

--- app/helpers/check_points_helper.rb
+++ app/helpers/check_points_helper.rb
@@ -2,11 +2,11 @@
 # CheckPointsHelper
 module CheckPointsHelper
   def edit_button_for_check_point(event, checkpoint)
-    link_to '編輯', edit_event_check_point_path(event, checkpoint), class: 'btn btn-primary' if policy(event).admin_or_event_manager?
+    link_to '編輯', edit_event_check_point_path(event, checkpoint), class: 'btn btn-primary' if policy(event).manage?
   end
 
   def delete_button_for_check_point(event, checkpoint)
-    state = policy(event).admin_or_event_manager?
-    link_to '刪除', event_check_point_path(event, checkpoint), method: 'delete', data: { confirm: '確認?' }, class: 'btn btn-danger' if state
+    avaliable = policy(event).manage?
+    link_to '刪除', event_check_point_path(event, checkpoint), method: 'delete', data: { confirm: '確認?' }, class: 'btn btn-danger' if avaliable
   end
 end
--- app/models/user_event_relationship.rb
+++ app/models/user_event_relationship.rb
@@ -2,9 +2,9 @@
 # User Event Relationship
 class UserEventRelationship < ApplicationRecord
   DEFAULT_PERMISSIONS = {
+    check_point_staff: false,
     write_check_point: false,
     write_attendee: false,
-    read_check_point: false,
     read_attendee: false
   }.freeze
   before_create { self.permissions = DEFAULT_PERMISSIONS.merge(permissions || {}) }
