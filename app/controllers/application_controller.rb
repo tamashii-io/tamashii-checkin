4b6cbf939731b28a2e00e381d5f444a5a822f179
jun <k5241284@kyoto-wu.ac.jp>
2017-08-24 19:30:21 +0900

    Fix NotAuthorizedError

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,16 @@
 class ApplicationController < ActionController::Base
   include Pundit
 
+  rescue_from Pundit::NotAuthorizedError, with: :deny_access
   before_action :authenticate_user!
+
+
+ protected
+
+  def deny_access
+   flash[:error] = 'access denied'
+   redirect_to action: :index
+  end
 
   protect_from_forgery with: :exception
 end
