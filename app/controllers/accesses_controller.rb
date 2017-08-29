f128accdb380dc936c97aa91d40a006a2719ef45
jun <k5241284@kyoto-wu.ac.jp>
2017-08-29 19:39:07 +0900

    fix conflict

--- app/controllers/accesses_controller.rb
+++ app/controllers/accesses_controller.rb
@@ -3,9 +3,7 @@
 # :nodoc:
 class AccessesController < ApplicationController
   before_action -> { @event = current_user.events.find(params[:event_id]) }
-  # before_action -> { @check_point = @event.check_points.gate.find_by(registrar: current_user)}
   before_action -> { @check_point = @event.check_points.gate.find_by(registrar: current_user) }
-  # before_action :pundit_auth
 
   def index
     # TODO: Reject user didn't have Gate checkpoint
