766c76b19399e715a62073d3cbb7bcb50078c21b
Liang-Chi Tseng <lctseng@cs.nctu.edu.tw>
2017-10-03 17:24:34 +0800

    shorten method length

--- app/middleware/event_auth_middleware.rb
+++ app/middleware/event_auth_middleware.rb
@@ -4,26 +4,15 @@ class EventAuthMiddleware
     @app = app
   end
 
-  # rubocop:disable Metrics/MethodLength
   def call(env)
     event_id = env['grape.routing_args'][:event_id]
-    if event_id
-      event = Event.find_by(id: event_id)
-      if event
-        headers = ActionDispatch::Http::Headers.from_hash(env)
-        if headers['Authorization'] == "Bearer #{event.api_token}"
-          @app.call(env)
-        else
-          response_error(401, 'EventTokenMismatch')
-        end
-      else
-        response_error(404, 'EventNotFound')
-      end
-    else
-      response_error(400, 'MissingEventId')
-    end
+    return response_error(400, 'MissingEventId') unless event_id
+    event = Event.find_by(id: event_id)
+    return response_error(404, 'EventNotFound') unless event
+    headers = ActionDispatch::Http::Headers.from_hash(env)
+    return response_error(401, 'EventTokenMismatch') unless headers['Authorization'] == "Bearer #{event.api_token}"
+    @app.call(env)
   end
-  # rubocop:enable Metrics/MethodLength
 
   def response_error(error_code, msg)
     [error_code, { 'Content-Type' => 'application/json' }, [{ error: msg }.to_json]]
