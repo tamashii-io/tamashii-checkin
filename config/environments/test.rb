d7b0a61120d81c28956812891b07b58aa06ed076
蒼時弦也 <elct9620@frost.tw>
2017-10-30 17:15:11 +0800

    Add mailer config for test environment

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -30,6 +30,10 @@ Rails.application.configure do
   config.action_controller.allow_forgery_protection = false
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = {
+    host: Settings.mailer.url.host
+  }
+
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
